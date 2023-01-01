9ba547cc2fabd59f9006a11f80b95f7b01f4f7e0
John Trady <docktor.mom19.93@gmail.com>
2023-01-01 17:51:38 +0300

    Task - 3
    Square Every Digit

--- js/tasks.js
+++ js/tasks.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// Task - 3 Square Every Digit  (from codewars.com)
+
+/**
+ * Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out, because 9**2 is 81 and 1**2 is 1.
+Note: The function accepts an integer and returns an integer
+ */
+
+
+const  squareDigits = num => {
+   let nums = num.toString().split('');
+   let result = [];
+   for(let i = 0; i < nums.length; i ++) {
+       result.push(nums[i]**2);
+   }
+    result = result.join('');
+    return Number(result);
+
+  
+
+   
+   
+};
+
+console.log(squareDigits(9119 ));
