18ded3afa827a4d32d1224199211bffd4aebbfaf
John Trady <docktor.mom19.93@gmail.com>
2023-01-08 22:14:07 +0300

    Task-10 Valid Parentheses from codewars.com (5 kyu)

--- js/tasks.js
+++ js/tasks.js
@@ -1,1 +1,34 @@
 'use strict';
+
+
+// Task-10 Valid Parentheses from codewars.com (5 kyu)
+
+
+/**
+ * Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+ */
+
+/**
+ * 
+ * @param {string} parens 
+ * @returns {string}
+ */
+
+const  validParentheses = parens => {
+  const stack= [];
+
+  for(const bracket of parens) {
+    if (bracket === '(') {
+       stack.push(')');
+    } else if ( stack.pop() !== bracket) {
+      return false;
+    }
+  }
+
+  return !stack.length;   
+}
+
+console.log(validParentheses('()'));  // true
+console.log(validParentheses(')(()))')); // false
+console.log(validParentheses('(')); // false
+console.log(validParentheses('(())((()())())')); // true 
