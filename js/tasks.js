92ce7d0c6f6be0c64fdc52dcd2d06e000bb8a5e4
John Trady <docktor.mom19.93@gmail.com>
2023-01-22 21:11:49 +0300

    Task-13 Moving Zeros To The End (5kyu)

--- js/tasks.js
+++ js/tasks.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// Task- ... ( kyu)
+// Task-13 Moving Zeros To The End (5kyu)
+
+
+/**
+ * Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+ */
+
+
+/**
+ * 
+ * @param {*} arr 
+ * @returns {Array}
+ */
+
+ const moveZeros = arr => {
+   for(let i = 0; i < arr.length; i++) {
+      let zeros = [];
+      for(const el of arr) {
+         if(el === 0) {
+            zeros.push(el);
+         }
+      }
+      arr = arr.filter(el => el !== 0);
+      arr = [...arr , ...zeros];
+      return arr;
+   }
+    return arr;
+ };
+
+ console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
